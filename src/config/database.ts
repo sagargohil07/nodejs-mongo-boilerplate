997f5c90600c343e35e47ae40c70c627e776f067
sagargohil07 <80506662+sagargohil07@users.noreply.github.com>
2026-02-07 17:02:30 +0530

    code refector

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,32 +1,11 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 export const connectDB = async (): Promise<void> => {
   try {
-    const mongoURI  = process.env.MONGODB_URI ;
-
+    const mongoURI = process.env.MONGODB_URI;
     await mongoose.connect(mongoURI as string);
-
-    console.log('✅ MongoDB connected successfully');
-    console.log(`📊 Database: ${mongoose.connection.name}`);
-
-    // Handle connection events
-    mongoose.connection.on('error', (err) => {
-      console.error('❌ MongoDB connection error:', err);
-    });
-
-    mongoose.connection.on('disconnected', () => {
-      console.log('⚠️  MongoDB disconnected');
-    });
-
-    // Graceful shutdown
-    process.on('SIGINT', async () => {
-      await mongoose.connection.close();
-      console.log('MongoDB connection closed through app termination');
-      process.exit(0);
-    });
-
+    mongoose.connection.on("error", (err) => {});
   } catch (error) {
-    console.error('❌ MongoDB connection failed:', error);
     process.exit(1);
   }
 };
--- src/middlewares/upload.middleware.ts
+++ src/middlewares/upload.middleware.ts
@@ -3,30 +3,28 @@ import path from "node:path";
 import { fileController } from "../controllers/file.controller";
 import { callbackify } from "node:util";
 
-
-//Storing in uploads directory 
+//Storing in uploads directory
 const storage = multer.diskStorage({
-    destination: 'uploads/',
-    filename(_req, file, callback) {
-        const fileName = file.originalname
-        callback(null, `${Date.now()}-${fileName}`)
-    },
-})
+  destination: "uploads/",
+  filename(_req, file, callback) {
+    const fileName = file.originalname;
+    callback(null, `${Date.now()}-${fileName}`);
+  },
+});
 
-//FileFiller 
-const fileFillter: multer.Options['fileFilter'] = (_req, file, callback) => {
-    const allowedTypes = ['application/pdf', 'text/csv'];
+//FileFiller
+const fileFillter: multer.Options["fileFilter"] = (_req, file, callback) => {
+  const allowedTypes = ["application/pdf", "text/csv"];
 
-    if (!allowedTypes.includes(file.mimetype)) {
-        return callback(new Error("only PDF and CSV file allowed"))
-    }
+  if (!allowedTypes.includes(file.mimetype)) {
+    return callback(new Error("only PDF and CSV file allowed"));
+  }
 
-    callback(null, true);
-}
-
+  callback(null, true);
+};
 
 export const uploadMiddleware = multer({
-    storage,
-    fileFilter: fileFillter,
-    limits: { fileSize: 2 * 1024 * 1024 }
-})
+  storage,
+  fileFilter: fileFillter,
+  limits: { fileSize: 2 * 1024 * 1024 },
+});
